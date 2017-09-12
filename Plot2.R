5890f55804e59365cc200a21430e7f84a321b22d
Chris Ward <chriswardchrisward@gmail.com>
2017-09-12 12:12:50 -0700

    added plot2.R code and plot2.png

---
+++ Plot2.R
@@ -0,0 +1,9 @@
+data = read.table("household_power_consumption.txt", header = TRUE, sep=";", dec= ".")
+mydata = data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+
+TimeDate <- strptime(paste(mydata$Date, mydata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+mydata <- cbind(mydata, TimeDate)
+
+png("plot2.png", width=480, height=480)
+with(mydata, plot(TimeDate, Global_active_power, type="l", xlab="Day", ylab="Global Active Power (kilowatts)"))
+dev.off()
