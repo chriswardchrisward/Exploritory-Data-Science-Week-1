a608a2659ee3186a5a3914fbb93aceeeb4655661
Chris Ward <chriswardchrisward@gmail.com>
2017-09-12 11:18:01 -0700

    Added code for plot1 and png of Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,2 +1,6 @@
-mydata = read.table("household_power_consumption.txt", header = TRUE)
-subset = mydata[mydata$Date %in% c("1/2/2007", "2/2/2007"),]
+data = read.table("household_power_consumption.txt", header = TRUE, sep=";", dec= ".")
+mydata = data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+Global_active_power <- as.numeric(as.character(mydata$Global_active_power))
+png("plot1.png", width=480, height=480)
+hist(Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
